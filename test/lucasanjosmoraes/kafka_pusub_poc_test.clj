c3f273d1c4f1c8e0a8f9485fd0261d789473494c
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2022-03-20 14:59:03 -0400

    chore: add default test file

---
+++ test/lucasanjosmoraes/kafka_pusub_poc_test.clj
@@ -0,0 +1,7 @@
+(ns lucasanjosmoraes.kafka-pusub-poc-test
+  (:require [clojure.test :refer :all]
+            [lucasanjosmoraes.kafka-pusub-poc :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
